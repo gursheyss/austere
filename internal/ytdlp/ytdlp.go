8d5b44727dc79ca78926b217cc88d2d0fe5e81c6
gursheyss <rkwarya@gmail.com>
2024-01-14 18:26:49 -0800

    lil refactor

--- internal/ytdlp/ytdlp.go
+++ internal/ytdlp/ytdlp.go
@@ -2,12 +2,35 @@ package ytdlp
 
 import (
 	"austere/internal/models"
-	"fmt"
+	"log"
 	"os/exec"
 	"strings"
 )
 
 func Download(params *models.BodyParams) error {
+	metadata := createMetadata(params)
+
+	cmd := exec.Command("yt-dlp", params.URL, "-x", "--audio-format", "mp3", "--add-metadata",
+		"--parse-metadata", "artist:%(artist||uploader)s",
+		"--parse-metadata", "title:%(title)s",
+		"--parse-metadata", "album:%(album)s",
+		"--replace-in-metadata", "album:NA=%(title)s",
+		"--ppa", "ThumbnailsConvertor+ffmpeg_o:-c:v png -vf crop='ih'",
+		"--ppa", "Metadata:"+metadata,
+		"--embed-thumbnail",
+	)
+
+	output, err := executeCommand(cmd)
+	if err != nil {
+		log.Println("Error executing yt-dlp:", err)
+		return err
+	}
+
+	log.Println(string(output))
+	return nil
+}
+
+func createMetadata(params *models.BodyParams) string {
 	metadata := []string{
 		"-metadata Track=\"\"",
 		"-metadata Year=\"\"",
@@ -16,29 +39,22 @@ func Download(params *models.BodyParams) error {
 	}
 
 	if params.Title != "" {
-		metadata = append(metadata, fmt.Sprintf("-metadata Title=\"%s\"", params.Title))
+		metadata = append(metadata, "-metadata Title=\""+params.Title+"\"")
 	}
 	if params.Album != "" {
-		metadata = append(metadata, fmt.Sprintf("-metadata Album=\"%s\"", params.Album))
+		metadata = append(metadata, "-metadata Album=\""+params.Album+"\"")
 	}
 	if params.Artist != "" {
-		metadata = append(metadata, fmt.Sprintf("-metadata Artist=\"%s\"", params.Artist))
+		metadata = append(metadata, "-metadata Artist=\""+params.Artist+"\"")
 	}
 
-	cmd := exec.Command("yt-dlp", params.URL, "-x", "--audio-format", "mp3", "--add-metadata",
-		"--parse-metadata", "artist:%(artist||uploader)s",
-		"--parse-metadata", "title:%(title)s",
-		"--parse-metadata", "album:%(album)s",
-		"--replace-in-metadata", "album:NA=%(title)s",
-		"--ppa", "ThumbnailsConvertor+ffmpeg_o:-c:v png -vf crop='ih'",
-		"--ppa", fmt.Sprintf("Metadata:%s", strings.Join(metadata, " ")),
-		"--embed-thumbnail",
-	)
+	return strings.Join(metadata, " ")
+}
 
+func executeCommand(cmd *exec.Cmd) ([]byte, error) {
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Error executing yt-dlp:", err)
+		return nil, err
 	}
-	fmt.Println(string(output))
-	return nil
+	return output, nil
 }
